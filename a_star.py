2ac5a760e4e243f8973c7fe2bb7495db76446dcc
pouria <pooriazmn@gmail.com>
2020-01-26 22:44:16 +0330

    before setting weight for fruit choices

---
+++ a_star.py
@@ -0,0 +1,96 @@
+
+def add(dic, key, value):
+    dic[key] = value
+
+
+class Node(object):
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+
+        self.g = 0
+        self.h = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    @property
+    def x(self):
+        return self.position[0]
+
+    @property
+    def y(self):
+        return self.position[1]
+
+    def calc_h(self, position):
+        if self.position is not None:
+            dx = abs(position[0] - self.position[0])
+            dy = abs(position[1] - self.position[1])
+
+            self.h = dx + dy
+
+    def calc_g(self):
+        if self.parent is None:
+            self.g = 0
+        else:
+            self.g = 1 + self.parent.g
+
+
+def A_Star_Search(maze, start, end, dont_visit):
+    print(maze)
+    start_node = Node(None, start)
+    end_node = Node(None, end)
+
+    if start == end:
+        return start
+
+    start_node.calc_h(end_node.position)
+    start_node.calc_g()
+
+    move = [[-1, 0],
+            [0, -1],
+            [1, 0],
+            [0, 1]]
+
+    to_visit = []
+    to_visit.append(start_node)
+    visited = []
+
+    while len(to_visit) > 0:
+        next_node_index = 0
+        next_node = to_visit[0]
+        f_next_node = next_node.h + next_node.g
+        for ind, each in enumerate(to_visit[1:], start=1):
+            if each.h + each.g < f_next_node:
+                next_node_index = ind
+                next_node = each
+                f_next_node = next_node.h + next_node.g
+
+        # print("F is => {} , Min value index: {}".format(dic, min(dic.keys(), key=(lambda k: dic[k]))))
+
+        current_node = to_visit.pop(next_node_index)
+        visited.append(current_node)
+        # print('current node pos ====> {}'.format(current_node.position))
+        if end_node == current_node:
+            path = []
+            while current_node.parent:
+                path.append(current_node)
+                current_node = current_node.parent
+            return path[::-1]
+
+        for m in move:
+            next_pos = (current_node.position[0] + m[0],
+                        current_node.position[1] + m[1])
+            if next_pos[0] < 0 or next_pos[0] >= len(maze) or next_pos[1] < 0 or next_pos[1] >= len(maze):
+                continue
+            print(f"running a*: {next_pos[0]}, {next_pos[1]}")
+            if maze[next_pos[0]][next_pos[1]] not in dont_visit:
+                node = Node(current_node, next_pos)
+                if node in visited:
+                    continue
+                node.calc_h(end_node.position)
+                node.calc_g()
+                to_visit.append(node)
--- bot.py
+++ bot.py
@@ -1,22 +1,138 @@
+import itertools
+import random
 import socket
 import struct
-import random
 from enum import Enum
+import a_star
+import json
 
 
 class Directions(Enum):
     UP, DOWN, LEFT, RIGHT = 'UP', 'DOWN', 'LEFT', 'RIGHT'
 
 
+class Fruits(Enum):
+    Orange, Banana, Cherry, WaterMelon, Apple = 'O', 'B', 'C', 'W', 'A'
+
+
 class AI:
 
     def __init__(self, bot_id, bot_count, board_size):
         self.__bot_id, self.__bot_count, self.__board_size = bot_id, bot_count, board_size
+        self.bot_fruits, self.board, self.fruits, = None, None, None
+        self.eaten = dict()
+        self.must_eat = dict()
+        self.init_eaten()
+        self.fruit_positions = dict()
+        self.x, self.y = 0, 0
+
+    def init_eaten(self):
+        for each in list(Fruits):
+            self.eaten[each] = 0
+
+        self.must_eat[Fruits.Orange] = 2
+        self.must_eat[Fruits.Apple] = 2
+        self.must_eat[Fruits.WaterMelon] = 1
+        self.must_eat[Fruits.Banana] = 1
+        self.must_eat[Fruits.Cherry] = 1
 
     def do_turn(self, board, bot_fruits):
         # write your bot's logic here
-        return random.choice(list(Directions))
+        self.board = board
+        self.bot_fruits = [None for _ in range(self.__bot_count)]
+        for each in bot_fruits:
+            self.bot_fruits.insert(int(each[0]), each[1:] if len(each) > 1 else [])
+        self.fruits = self.bot_fruits[self.__bot_id]
+        self.find_fruits()
+        self.set_eaten()
+        self.get_position()
+        return self.best_action()
 
+    def best_action(self):
+        f_order = self.fruit_order()
+        min_path = None
+        min_path_len = float('+inf')
+        check, district = "", '*'
+        for f, w in f_order:
+            if w > 0:
+                check += f
+            else:
+                district += f
+        for f in check:
+            min_path_f = None
+            min_path_len_f = float('+inf')
+            for pos in self.fruit_positions[f]:
+                path = a_star.A_Star_Search(self.board, (self.x, self.y), pos, district)
+                if len(path) < min_path_len_f:
+                    min_path_f = path
+                    min_path_len_f = len(path)
+            if min_path_len_f < min_path_len:
+                min_path_len = min_path_len_f
+                min_path = min_path_f
+
+        print(f'({self.x}, {self.y}) -> {min_path[0]}')
+        ac = self.get_action(min_path[0])
+        print(ac)
+        return ac
+
+    def get_action(self, pos):
+        if pos.x - self.x == 1:
+            return Directions.DOWN
+        elif pos.x - self.x == -1:
+            return Directions.UP
+        elif pos.y - self.y == 1:
+            return Directions.RIGHT
+        elif pos.y - self.y == -1:
+            return Directions.LEFT
+        else:
+            print(f'Not suportet pos --> {pos} for current {self.x, self.y}')
+
+    def find_fruits(self):
+        for f in 'OABCW':
+            self.fruit_positions[f] = list()
+        for x, y in itertools.product(range(self.__board_size), range(self.__board_size)):
+            if self.board[x][y] in 'OABCW':
+                self.fruit_positions[self.board[x][y]].append((x, y))
+
+        """for key, value in self.fruit_positions.items():
+            print(key, ', '.join(map(str, value)), sep=':\t')"""
+
+    def get_position(self):
+        for x, row in enumerate(self.board):
+            y = row.find(str(self.__bot_id))
+            if y >= 0:
+                self.x, self.y = x, y
+
+    def set_eaten(self):
+        print("this bot fruits: ", self.fruits)
+        for each in self.fruits:
+            if each == Fruits.Orange.value:
+                self.eaten[Fruits.Orange] += 1
+            elif each == Fruits.Apple.value:
+                self.eaten[Fruits.Apple] += 1
+            elif each == Fruits.Banana.value:
+                self.eaten[Fruits.Banana] += 1
+            elif each == Fruits.Cherry.value:
+                self.eaten[Fruits.Cherry] += 1
+            elif each == Fruits.WaterMelon.value:
+                self.eaten[Fruits.WaterMelon] += 1
+            else:
+                print(each)
+                raise Exception
+
+    def set_must_eat(self):
+        self.must_eat[Fruits.Orange] = max(self.must_eat[Fruits.Orange],
+                                           self.eaten[Fruits.Banana]*2 - self.eaten[Fruits.Orange])
+        self.must_eat[Fruits.Apple] = max(self.must_eat[Fruits.Apple],
+                                          self.eaten[Fruits.Cherry] + self.eaten[Fruits.WaterMelon] - self.eaten[Fruits.Apple])
+        self.must_eat[Fruits.WaterMelon] = self.must_eat[Fruits.WaterMelon] - self.eaten[Fruits.WaterMelon]
+        self.must_eat[Fruits.Cherry] = self.must_eat[Fruits.Cherry] - self.eaten[Fruits.Cherry]
+        self.must_eat[Fruits.Banana] = self.must_eat[Fruits.Banana] - self.eaten[Fruits.Banana]
+
+    def fruit_order(self):
+        self.set_must_eat()
+        return list(map(lambda x: (getattr(x, 'value'), self.must_eat[x]),
+                        sorted(list(Fruits), key=lambda x: self.must_eat[x])))
 
 def read_utf(sock: socket.socket):
     length = struct.unpack('>H', s.recv(2))[0]
@@ -38,5 +154,6 @@ if __name__ == '__main__':
     while True:
         board_str = read_utf(s)
         board = [board_str[i * board_size:(i + 1) * board_size] for i in range(board_size)]
+        print('\n'.join(board), end='\n-------------------------\n')
         fruits = [read_utf(s) for _ in range(bot_count)]
         write_utf(s, ai.do_turn(board, fruits).value)
